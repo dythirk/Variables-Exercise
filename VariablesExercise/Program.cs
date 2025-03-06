1082a0073ae8cefc1ca84eede1bcd797e4aa13ec
Thai Vo <tahrbo@gmail.com>
2025-03-06 01:22:14 -0600

    Wrote code

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string lastName = "Bond";
+            char initial = 'J';
+            int numMovies = 27;
+            bool beingProduced = true;
+            double totalTickets = 500;
+            decimal totalGrosses = 7000000000.00m;
+
+
+
+            Console.WriteLine($"My name is {lastName}, and my first inital is {initial}.");
+            Console.WriteLine($"I have had {numMovies} movies made about my character.");
+            Console.WriteLine($"My films have sold an estimated {totalTickets} million tickets and grossed over {totalGrosses} dollars.");
+            Console.WriteLine($"Are my movies still being produced? {beingProduced}.");
         }
     }
 }
